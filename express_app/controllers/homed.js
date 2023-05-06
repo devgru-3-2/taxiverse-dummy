ec82b0150c3fe4c5ad30278062525fd7b608f5dd
devgru-3-2 <95485002+devgru-3-2@users.noreply.github.com>
2023-05-07 04:26:17 +0530

    Update homed.js

--- express_app/controllers/homed.js
+++ express_app/controllers/homed.js
@@ -73,7 +73,7 @@ module.exports = (app) => {
         const customerUsername = req.body.username;
         const value = req.body.value;
         const provider = new HDwalletprovider(
-            "ccdddeb92b1f4367e837ca8adf3fd128a433b4737960013946b2d18263ea7781",
+            "...",
             'https://sepolia.infura.io/v3/3bd9ec3cd7924268a521a9ab04f95da8'
         );
 
@@ -146,7 +146,7 @@ module.exports = (app) => {
     app.post("/finald", async (req, res) => {
 
         const provider = new HDwalletprovider(
-            "ccdddeb92b1f4367e837ca8adf3fd128a433b4737960013946b2d18263ea7781",
+            "...",
             'https://sepolia.infura.io/v3/3bd9ec3cd7924268a521a9ab04f95da8'
         );
         const web = new Web3(provider);
